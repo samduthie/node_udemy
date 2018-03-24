93e831e99567d7bc642ce268451ea7f6b8dd9722
Sam <samduthie@gmail.com>
2018-03-24 14:12:25 +0000

    cleanup

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
 
-console.log("starting app");
-
-// console.log(_.isString(true));
-// console.log(_.isString('Andrew'));
-
-
-var filteredArray = _.uniq([1,2,1, 'test','test']);
-console.log(filteredArray);
