102583a7e0ad2b9fea409d449e5400588295d02b
Sam <samduthie@gmail.com>
2018-03-24 14:45:37 +0000

    Playground JSON

---
+++ notes-node/playground/json.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+var originalNote = {
+    title: 'Some title',
+    body: 'Some body'
+};
+
+originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json')
+var note = JSON.parse(noteString)
+
+console.log(typeof note);
+console.log(note.title)
