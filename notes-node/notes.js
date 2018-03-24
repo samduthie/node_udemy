302dc910da159890b3f93fcb7af1446eb3ba1f8b
Sam <samduthie@gmail.com>
2018-03-24 15:05:06 +0000

    Add notes will save into JSON file

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,8 +1,28 @@
 console.log("starting notes.js")
 
+const fs = require('fs');
 
 var addNote = (title, body) => {
-    console.log(title, body)
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString)
+    } catch (e) {
+
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length ===0){
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+
 };
 
 var getAll = () => {
