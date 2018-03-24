009d076c61aa76ec0d36ad2a558ec3b8bf23c108
Sam <samduthie@gmail.com>
2018-03-24 14:09:57 +0000

    Using 3rd party modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,24 +1,14 @@
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
+
 console.log("starting app");
-var user = os.userInfo();
+
+// console.log(_.isString(true));
+// console.log(_.isString('Andrew'));
 
 
-var res = notes.addNote();
-
-console.log(res);
-console.log('result: ', notes.add(21,4));
-
-// var username = user.username
-// console.log(user.username)
-
-// fs.appendFile('greetings.txt', 'Hello ' + username +'! You are ' + notes.age, function (err) { //callback
-//     if (err) {
-//         console.log("unable to write to file");
-//     }
-// });
-
-
-// console.log(`Hello ${username}!`) //js template string
+var filteredArray = _.uniq([1,2,1, 'test','test']);
+console.log(filteredArray);
