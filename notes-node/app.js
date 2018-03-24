481116aed0e42d025e647dcef819a35e9f7f9498
Sam <samduthie@gmail.com>
2018-03-24 14:21:31 +0000

    command line args

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,4 +3,18 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+var command= process.argv[2];
+console.log(command);
+console.log(process.argv)
 
+if (command === 'add'){
+    console.log('adding new note');
+} else if (command === 'list'){
+    console.log('listing all notes')
+} else if (command === 'remove'){
+    console.log('removing note')
+} else if (command === 'read'){
+    console.log('reading note')
+} else {
+    console.log('command not recognised')
+}
